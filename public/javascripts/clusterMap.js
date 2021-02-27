5979fe244e0104595770717a37e89119ef154749
Ludvig Sellebråten <l.sellebraten@gmail.com>
2021-02-27 09:48:54 +0100

    Changed start point of map

--- public/javascripts/clusterMap.js
+++ public/javascripts/clusterMap.js
@@ -2,8 +2,8 @@ mapboxgl.accessToken = mapToken
 var map = new mapboxgl.Map({
 container: 'cluster-map',
 style: 'mapbox://styles/mapbox/light-v10',
-center: [-103.59179687498357, 40.66995747013945],
-zoom: 3
+center: [18.072374, 59.309095],
+zoom: 4
 });
 
 map.addControl(new mapboxgl.NavigationControl());
